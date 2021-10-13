5dce8e871f22ea5fbaa8ae85c2ae95719dbfe7bd
lpflpf <lipengfei634626165@163.com>
2021-10-13 14:07:39 +0800

    fix in mobile

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,54 @@
 <template>
-  <div id="app">
-    <el-header style="padding: 0">
-      <el-menu
-        class="el-menu-demo"
-        mode="horizontal"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b"
-        default-active="1"
-        router="true"
-      >
+    <div id="app">
+        <el-header style="padding: 0">
+            <el-menu
+                class="el-menu-demo"
+                mode="horizontal"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b"
+                default-active="1"
+                router="true"
+                >
 
-        <el-menu-item index="1" :route="{name: 'Sql2Go'}">SQL转Struct</el-menu-item>
-        <el-menu-item index="2" :route="{name: 'Json2Go'}">Json转Struct</el-menu-item>
-        <el-menu-item index="3" :route="{name:'FormatJson'}">Json格式化</el-menu-item>
+                <el-menu-item index="1" :route="{name: 'Sql2Go'}">SQL转Struct</el-menu-item>
+                <el-menu-item index="2" :route="{name: 'Json2Go'}">Json转Struct</el-menu-item>
+                <el-menu-item index="3" :route="{name:'FormatJson'}">Json格式化</el-menu-item>
 
-        <el-menu-item style="float:right"><a href="https://blog.lpflpf.cn">博客</a></el-menu-item>
-        <el-menu-item style="float:right"><a href="https://github.com/lpflpf">github</a></el-menu-item>
-      </el-menu>
-    </el-header>
-    <router-view/>
-  </div>
+                <el-menu-item style="float:right"><a href="https://blog.lpflpf.cn">博客</a></el-menu-item>
+                <el-menu-item style="float:right"><a href="https://github.com/lpflpf">github</a></el-menu-item>
+            </el-menu>
+        </el-header>
+        <router-view/>
+    </div>
 </template>
 
 <script>
 /* eslint-disable */
-  export default {
-    name: 'App'
-  }
+export default {
+    name: 'App',
+    metaInfo: {
+        meta: [
+            { 
+                name: "viewport", 
+                content: "width=device-width, initial-scale=0,user-scalable=no,maximum-scale=1.0"
+            },
+        ]
+    }
+}
 </script>
 
 <style>
-  #app {
+#app {
 
-  }
+}
 
-  a {
+a {
     text-decoration: blink;
-  }
+}
 
-  body {
-      margin: 0px;
-  }
+body {
+    margin: 0px;
+}
 
 </style>
